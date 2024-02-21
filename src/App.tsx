5a18c55a281bd8ef509b915f824252867dd09696
Guilherme Carvalho <guilherme.carvalhos@outlook.com>
2024-02-21 19:50:39 -0300

    feature(components): button, forms and list

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import React from "react";
+import Forms from "./components/forms";
+import List from "./components/list";
 
 function App() {
-  return <div className="App">
-  </div>;
+  return (
+    <div className="App">
+      <Forms />
+      <List />
+    </div>
+  );
 }
 
 export default App;
