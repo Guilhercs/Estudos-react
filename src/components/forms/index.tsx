ade53d9dc6056081fde8ed30d92e9d9d43c4bbc0
Guilherme Carvalho <guilherme.carvalhos@outlook.com>
2024-02-25 21:23:40 -0300

    chore: setState todo

--- src/components/forms/index.tsx
+++ src/components/forms/index.tsx
@@ -3,15 +3,31 @@ import Button from "../button";
 import formsStyle from "./forms-style.module.scss";
 
 export default class Formulario extends React.Component {
+  state = {
+    todo: "",
+    tempo: "00:00",
+  };
+
+  addTodo(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    console.log("state: ", this.state);
+  }
   render() {
     return (
-      <form className={formsStyle.novaTarefa}>
+      <form
+        className={formsStyle.novaTarefa}
+        onSubmit={this.addTodo.bind(this)}
+      >
         <div className={formsStyle.inputContainer}>
           <label htmlFor="tarefa">Todo: </label>
           <input
             type="text"
             name="tarefa"
             id="tarefa"
+            value={this.state.todo}
+            onChange={(event) =>
+              this.setState({ ...this.state, todo: event.target.value })
+            }
             placeholder="Drink Wather"
             required
           />
@@ -25,6 +41,10 @@ export default class Formulario extends React.Component {
             type="time"
             step="1"
             name="tempo"
+            value={this.state.tempo}
+            onChange={(event) =>
+              this.setState({ ...this.state, tempo: event.target.value })
+            }
             id="tempo"
             min="00:00:00"
             max="01:30:00"
